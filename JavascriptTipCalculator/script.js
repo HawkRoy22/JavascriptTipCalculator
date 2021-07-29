9587431ccf677a918d9f3c274ec9671b8fce92c2
Aneek <44777979+HawkRoy22@users.noreply.github.com>
2021-07-29 13:31:53 -0500

    Add files via upload

---
+++ JavascriptTipCalculator/script.js
@@ -0,0 +1,20 @@
+let bill;
+let numPeople;
+const ratePoor = 1.1;
+const rateDecent = 1.15;
+const rateGreat = 1.2;
+
+let x = prompt("What is your bill amount? ")
+
+function billAmount(){
+    console.log(x * 1.15)
+}
+
+function burh(){
+    
+}
+
+
+
+
+
